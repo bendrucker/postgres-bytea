3096e8a962cb8d7847edee00a9e03bf76b3ba49e
Panu Pitkamaki <pspi@users.noreply.github.com>
2020-06-05 08:05:27 +0300

    Add more bytea to binary tests

--- test/byteaToBinary.test.js
+++ test/byteaToBinary.test.js
@@ -3,8 +3,12 @@ const assert = require('assert')
 const { byteaToBinary } = require('..')
 
 describe('bytea to binary', () => {
-  it('handles pg <9 escape syntax', () => {
+  it('handles pg <9 escape format', () => {
     const buffer = Buffer.from([102, 111, 111, 0, 128, 92, 255])
     assert(buffer.equals(byteaToBinary('foo\\000\\200\\\\\\377')))
+  })
+
+  it('handles pg >=9 hex format', () => {
+    assert(byteaToBinary('\\x1234').equals(Buffer.from([0x12, 0x34])))
   })
 })
