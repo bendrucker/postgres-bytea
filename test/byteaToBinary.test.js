ccf8a5822240b1e568321e8d3b126de6eb65c643
Panu Pitkamaki <pspi@users.noreply.github.com>
2020-06-05 07:47:19 +0300

    Reorganize production and test code

--- test/byteaToBinary.test.js
+++ test/byteaToBinary.test.js
@@ -2,10 +2,10 @@
 
 const test = require('tape')
 
-const bytea = require('./')
+const { byteaToBinary } = require('..')
 
 test((t) => {
   const buffer = Buffer.from([102, 111, 111, 0, 128, 92, 255])
-  t.ok(buffer.equals(bytea('foo\\000\\200\\\\\\377')))
+  t.ok(buffer.equals(byteaToBinary('foo\\000\\200\\\\\\377')))
   t.end()
 })
