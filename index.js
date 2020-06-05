8f8b1cde039c48622901d6fd8eb34c0afb5cc95e
Panu Pitkamaki <pspi@users.noreply.github.com>
2020-06-05 07:47:19 +0300

    Reorganize production and test code

--- index.js
+++ index.js
@@ -1,31 +1,6 @@
 'use strict'
 
-module.exports = function parseBytea (input) {
-  if (/^\\x/.test(input)) {
-    // new 'hex' style response (pg >9.0)
-    return Buffer.from(input.substr(2), 'hex')
-  }
-  let output = ''
-  let i = 0
-  while (i < input.length) {
-    if (input[i] !== '\\') {
-      output += input[i]
-      ++i
-    } else {
-      if (/[0-7]{3}/.test(input.substr(i + 1, 3))) {
-        output += String.fromCharCode(parseInt(input.substr(i + 1, 3), 8))
-        i += 4
-      } else {
-        let backslashes = 1
-        while (i + backslashes < input.length && input[i + backslashes] === '\\') {
-          backslashes++
-        }
-        for (let k = 0; k < Math.floor(backslashes / 2); ++k) {
-          output += '\\'
-        }
-        i += Math.floor(backslashes / 2) * 2
-      }
-    }
-  }
-  return Buffer.from(output, 'binary')
-}
+const byteaToBinary = require('./byteaToBinary')
+
+module.exports = byteaToBinary // default export for backwards compatibility
+module.exports.byteaToBinary = byteaToBinary
--- test/byteaToBinary.test.js
+++ test/byteaToBinary.test.js
@@ -1,12 +1,10 @@
-'use strict'
-
 const assert = require('assert')
 
-const bytea = require('./')
+const { byteaToBinary } = require('..')
 
 describe('bytea to binary', () => {
   it('handles pg <9 escape syntax', () => {
     const buffer = Buffer.from([102, 111, 111, 0, 128, 92, 255])
-    assert(buffer.equals(bytea('foo\\000\\200\\\\\\377')))
+    assert(buffer.equals(byteaToBinary('foo\\000\\200\\\\\\377')))
   })
 })
