97a77e1eeae9e23523a8372a1d15806319b22e36
Panu Pitkamaki <pspi@users.noreply.github.com>
2020-06-05 07:34:40 +0300

    Modernize used JavaScript

--- test.js
+++ test.js
@@ -1,10 +1,11 @@
 'use strict'
 
-var test = require('tape')
-var bytea = require('./')
+const test = require('tape')
 
-test(function (t) {
-  var buffer = Buffer.from([102, 111, 111, 0, 128, 92, 255])
+const bytea = require('./')
+
+test((t) => {
+  const buffer = Buffer.from([102, 111, 111, 0, 128, 92, 255])
   t.ok(buffer.equals(bytea('foo\\000\\200\\\\\\377')))
   t.end()
 })
