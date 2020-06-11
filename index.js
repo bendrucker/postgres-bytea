2570abf10e6e7891c2b4336b8c921ed1ef6d068e
Panu Pitkamaki <pspi@users.noreply.github.com>
2020-06-11 20:24:29 +0300

    Add stream encoder/decoder (#9)
    Breaking: require node 8
    
    Co-authored-by: Ben Drucker <bvdrucker@gmail.com>

--- index.js
+++ index.js
@@ -1,31 +1,6 @@
 'use strict'
 
-module.exports = function parseBytea (input) {
-  if (/^\\x/.test(input)) {
-    // new 'hex' style response (pg >9.0)
-    return Buffer.from(input.substr(2), 'hex')
-  }
-  var output = ''
-  var i = 0
-  while (i < input.length) {
-    if (input[i] !== '\\') {
-      output += input[i]
-      ++i
-    } else {
-      if (/[0-7]{3}/.test(input.substr(i + 1, 3))) {
-        output += String.fromCharCode(parseInt(input.substr(i + 1, 3), 8))
-        i += 4
-      } else {
-        var backslashes = 1
-        while (i + backslashes < input.length && input[i + backslashes] === '\\') {
-          backslashes++
-        }
-        for (var k = 0; k < Math.floor(backslashes / 2); ++k) {
-          output += '\\'
-        }
-        i += Math.floor(backslashes / 2) * 2
-      }
-    }
-  }
-  return Buffer.from(output, 'binary')
-}
+exports = module.exports = require('./decode')
+
+exports.Encoder = require('./encoder')
+exports.Decoder = require('./decoder')
