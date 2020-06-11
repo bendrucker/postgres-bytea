9692298b238f65da562d34a7d7a47708f81328e7
Ben Drucker <bvdrucker@gmail.com>
2020-06-10 18:08:30 -0700

    remove assertConvertsTo

--- decoder-test.js
+++ decoder-test.js
@@ -2,20 +2,18 @@
 
 const test = require('tape-promise').default(require('tape'))
 
-const { assertConvertsTo } = require('./test-support')
+const { Readable } = require('stream')
+const streamToPromise = require('stream-to-promise')
 const Decoder = require('./decoder')
 
 test('decoder', (t) => {
-  t.test('input cuts at chunk boundary multiple ways', async (t) => {
+  t.test('input cuts at chunk boundary', async (t) => {
     const input = [...Buffer.from('\\\\x616263').values()]
 
     for (let i = 1; i < input.length; i++) {
-      const inputPart1 = input.slice(0, i)
-      const inputPart2 = input.slice(i)
-
-      await assertConvertsTo(t, [inputPart1, inputPart2], new Decoder(), 'abc')
+      const result = await streamToPromise(Readable.from([input.slice(0, i), input.slice(i)].map(Buffer.from)).pipe(new Decoder()))
+      t.equal(result.toString(), 'abc', `i=${i}`)
     }
-    t.end()
   })
 
   t.test('fails if not prefixed with \\\\x', (t) => {
